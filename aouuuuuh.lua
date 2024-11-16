0cf33880cd6c2c1324ccbca36268191b45902bef
mijikaisys <mikifemboy@gmail.com>
2024-11-16 22:03:58 +0100

    Update aouuuuuh.lua

--- aouuuuuh.lua
+++ aouuuuuh.lua
@@ -1,1 +1,10 @@
+local button = script.Parent
 
+button.Size = UDim2.new(0, 200, 0, 50) -- Taille du bouton
+button.Position = UDim2.new(0.5, -100, 0.5, -25) -- Position du bouton au centre de l'écran
+button.Text = "Cliquez ici" -- Texte du bouton
+
+button.MouseButton1Click:Connect(function()
+    print("Bouton cliqué !") -- Action à réaliser lorsque le bouton est cliqué
+    button.Text = "Vous avez cliqué !" -- Changer le texte du bouton après le clic
+end)
