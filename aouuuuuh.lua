8b4b92c372147b7e7720a34498322d0fe5efac9b
mijikaisys <mikifemboy@gmail.com>
2024-11-16 22:10:15 +0100

    Update aouuuuuh.lua

--- aouuuuuh.lua
+++ aouuuuuh.lua
@@ -1,10 +1,40 @@
-local button = script.Parent
+game:GetService("Players").LocalPlayer:WaitForChild("PlayIt")
+local function fireHitRemote()
+    if debounce then return end
+    debounce = true
+    delay(0.01, function() debounce = false end)
 
-button.Size = UDim2.new(0, 200, 0, 50) -- Taille du bouton
-button.Position = UDim2.new(0.5, -100, 0.5, -25) -- Position du bouton au centre de l'écran
-button.Text = "Cliquez ici" -- Texte du bouton
+    local args = {
+    [1] = "Howl"
+}
 
-button.MouseButton1Click:Connect(function()
-    print("Bouton cliqué !") -- Action à réaliser lorsque le bouton est cliqué
-    button.Text = "Vous avez cliqué !" -- Changer le texte du bouton après le clic
-end)
+game:GetService("Players").LocalPlayer:WaitForChild("PlayIt"):FireServer(unpack(args))
+
+        fireHitRemote()
+    end
+end
+-- Fonction pour créer le bouton
+local function createButton()
+    -- Vérifiez si le bouton existe déjà et le supprimez si c'est le cas
+    if Button then
+        Button:Destroy()
+    end
+
+    -- Créez un nouveau ScreenGui et un bouton
+    local ScreenGui = Instance.new("ScreenGui")
+    Button = Instance.new("TextButton")
+
+    -- Configuration du ScreenGui
+    ScreenGui.Parent = Player:WaitForChild("PlayerGui")
+
+    -- Configuration du bouton
+    Button.Size = UDim2.new(0, 100, 0, 50) -- Taille du bouton
+    Button.Position = UDim2.new(0.5, 200, 0.5, -25) -- Position du bouton
+    Button.Text = "Fire Hit Remote"
+    Button.BackgroundColor3 = Color3.fromRGB(0, 170, 0) -- Couleur de fond
+    Button.TextColor3 = Color3.new(1, 1, 1) -- Couleur du texte
+    Button.Parent = ScreenGui
+
+    -- Connecter la fonction au clic du bouton
+    Button.MouseButton1Click:Connect(fireHitRemote)
+end
